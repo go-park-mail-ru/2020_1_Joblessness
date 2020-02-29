120c9b41f3e756daee224e05c0674fad511d10d9
maratishimbaev <maratishimbaev8@gmail.com>
2020-02-29 12:35:32 +0300

    Splited user.go to two files

--- models/user.go
+++ models/user.go
@@ -15,19 +15,3 @@ type UserLogin struct {
 	Login string `json:"login"`
 	Password string `json:"password"`
 }
-
-type UserPage struct {
-	User interface{} `json:"user,omitempty"`
-	Summaries interface{} `json:"summaries"`
-}
-
-type UserInfo struct {
-	Firstname string `json:"first-name,omitempty"`
-	Lastname string `json:"last-name,omitempty"`
-	Tag string `json:"tag,omitempty"`
-	Avatar string `json:"avatar,omitempty"`
-}
-
-type UserSummary struct {
-	Title string `json:"title"`
-}
