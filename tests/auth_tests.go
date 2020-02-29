35772d7d37c41581b7bec433ca19862c44788093
maratishimbaev <maratishimbaev8@gmail.com>
2020-02-29 11:50:47 +0300

    Split users_test.go to two files

---
+++ tests/auth_tests.go
@@ -0,0 +1,229 @@
+package tests
+
+import (
+	_h "../handlers"
+	_models "../models"
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestLogin(t *testing.T) {
+	t.Parallel()
+
+	h := _h.NewAuthHandler()
+
+	body := bytes.NewReader([]byte(`{"login": "marat1k", "password": "ABCDE12345"}`))
+
+	r := httptest.NewRequest("POST", "/api/users/login", body)
+	w := httptest.NewRecorder()
+
+	h.Login(w, r)
+
+	if w.Code != http.StatusCreated {
+		t.Error("status is not 201")
+	}
+
+	if w.Result().Cookies()[0].Name == "session-id" {
+		t.Error("Cookie wasnt received")
+	}
+
+	if len(h.Sessions) != 1 {
+		t.Error("Cookie wasnt saved")
+	}
+}
+
+func TestFailedLoginNotFound(t *testing.T) {
+	t.Parallel()
+
+	h := _h.NewAuthHandler()
+
+	body := bytes.NewReader([]byte(`{"login": "maratk", "password": "ABE12345"}`))
+
+	r := httptest.NewRequest("POST", "/api/users/login", body)
+	w := httptest.NewRecorder()
+
+	h.Login(w, r)
+
+	if w.Code != http.StatusNotFound {
+		t.Error("status is not 404")
+	}
+
+	if len(w.Result().Cookies()) != 0 {
+		t.Error("Wrong Cookie was received")
+	}
+
+	if len(h.Sessions) == 1 {
+		t.Error("Wrong Cookie wasnt saved")
+	}
+}
+
+func TestFailedLoginWrongPassword(t *testing.T) {
+	t.Parallel()
+
+	h := _h.NewAuthHandler()
+
+	body := bytes.NewReader([]byte(`{"login": "marat1k", "password": "ABE12345"}`))
+
+	r := httptest.NewRequest("POST", "/api/users/login", body)
+	w := httptest.NewRecorder()
+
+	h.Login(w, r)
+
+	if w.Code != http.StatusBadRequest {
+		t.Error("status is not 400")
+	}
+
+	if len(w.Result().Cookies()) != 0 {
+		t.Error("Wrong Cookie was received")
+	}
+
+	if len(h.Sessions) == 1 {
+		t.Error("Wrong Cookie wasnt saved")
+	}
+}
+
+func TestLogout(t *testing.T) {
+	t.Parallel()
+
+	h := _h.NewAuthHandler()
+	h.Sessions["marat1k"] = 1
+
+	body := bytes.NewReader([]byte{})
+
+	r := httptest.NewRequest("POST", "/api/users/logout", body)
+	cookie := &http.Cookie {
+		Name: "session_id",
+		Value: "marat1k",
+		Expires: time.Now().Add(time.Hour),
+	}
+	r.AddCookie(cookie)
+	w := httptest.NewRecorder()
+
+	h.Logout(w, r)
+
+	if w.Code != http.StatusCreated{
+		t.Error("status is not 201")
+	}
+
+	if len(h.Sessions) != 0 {
+		t.Error("Session wasnt closed")
+	}
+}
+
+func TestLogoutWrongCookie(t *testing.T) {
+	t.Parallel()
+
+	h := _h.NewAuthHandler()
+	h.Sessions["marat1k"] = 1
+
+	body := bytes.NewReader([]byte{})
+
+	r := httptest.NewRequest("POST", "/api/users/logout", body)
+	cookie := &http.Cookie {
+		Name: "session_id",
+		Value: "mart1k",
+		Expires: time.Now().Add(time.Hour),
+	}
+	r.AddCookie(cookie)
+	w := httptest.NewRecorder()
+
+	h.Logout(w, r)
+
+	if w.Code != http.StatusUnauthorized{
+		t.Error("status is not 401")
+	}
+
+	if len(h.Sessions) == 0 {
+		t.Error("Wrong session was closed")
+	}
+}
+
+func TestLogoutNoCookie(t *testing.T) {
+	t.Parallel()
+
+	h := _h.NewAuthHandler()
+	h.Sessions["marat1k"] = 1
+
+	body := bytes.NewReader([]byte{})
+
+	r := httptest.NewRequest("POST", "/api/users/logout", body)
+	w := httptest.NewRecorder()
+
+	h.Logout(w, r)
+
+	if w.Code != http.StatusUnauthorized {
+		t.Error("status is not 401")
+	}
+
+	if len(h.Sessions) == 0 {
+		t.Error("Wrong session was closed")
+	}
+}
+
+func TestRegistration(t *testing.T) {
+	t.Parallel()
+
+	h := _h.NewAuthHandler()
+
+	//TODO Не забывать пробелы!!!
+	body := bytes.NewReader([]byte(`{"login": "huvalk", 
+"password": "ABE12345", 
+"first-name": "first", 
+"last-name": "last", 
+"email": "m@m.m", 
+"phone-number": "89032909812"
+}`))
+
+	r := httptest.NewRequest("POST", "/api/users", body)
+	w := httptest.NewRecorder()
+
+	h.Register(w, r)
+
+	if w.Code != http.StatusCreated {
+		t.Error("status is not 201")
+	}
+
+	expectedUser := _models.User{
+		ID: 2,
+		Login: "huvalk",
+		Password: "ABE12345",
+		FirstName: "first",
+		LastName: "last",
+		Email: "m@m.m",
+		PhoneNumber: "89032909812",
+	}
+
+	reflect.DeepEqual(h.Users["huvalk"], expectedUser)
+}
+
+func TestFailedRegistration(t *testing.T) {
+	t.Parallel()
+
+	h := _h.NewAuthHandler()
+
+	//TODO Не забывать пробелы!!!
+	body := bytes.NewReader([]byte(`{"login": "marat1k", 
+"password": "ABE12345", 
+"first-name": "first", 
+"last-name": "last", 
+"email": "m@m.m", 
+"phone-number": "89032909812"
+}`))
+
+	r := httptest.NewRequest("POST", "/api/users", body)
+	w := httptest.NewRecorder()
+
+	h.Register(w, r)
+
+	if w.Code != http.StatusBadRequest {
+		t.Error("status is not 400")
+	}
+
+	if len(h.Users) != 1 {
+		t.Error("Wrong user was created")
+	}
+}
