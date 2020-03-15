bb55ac6ea170b90a139a5f525882d7bd4b425f02
Куклин С. И <akiraq@yandex.ru>
2020-03-15 11:53:17 +0300

    feat(Organization): orgs endpoints registered

--- haha/auth/delivery/http/register.go
+++ haha/auth/delivery/http/register.go
@@ -13,6 +13,8 @@ func RegisterHTTPEndpoints(router *mux.Router, uc auth.UseCase) {
 	router.HandleFunc("/users/logout", h.Logout).Methods("POST")
 	router.HandleFunc("/users", h.RegisterPerson).Methods("POST")
 	router.HandleFunc("/organizations", h.RegisterOrg).Methods("POST")
-	router.HandleFunc("/users/{user_id}", h.GetPerson).Methods("GET")
-	router.HandleFunc("/users/{user_id}", h.ChangePerson).Methods("PUT")
+	router.HandleFunc("/users/{user_id:[0-9]+}", h.GetPerson).Methods("GET")
+	router.HandleFunc("/users/{user_id:[0-9]+}", h.ChangePerson).Methods("PUT")
+	router.HandleFunc("/organizations/{user_id:[0-9]+}", h.GetOrganization).Methods("GET")
+	router.HandleFunc("/organizations/{user_id:[0-9]+}", h.ChangeOrganization).Methods("PUT")
 }
