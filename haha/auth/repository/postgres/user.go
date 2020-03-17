1bf553a2da9f4100734f84a7d24b72b891a23db2
Куклин С. И <akiraq@yandex.ru>
2020-03-17 12:38:59 +0300

    debug auth

--- haha/auth/repository/postgres/user.go
+++ haha/auth/repository/postgres/user.go
@@ -171,7 +171,7 @@ func (r UserRepository) CreatePerson(user *models.Person) (err error) {
 
 	var personId uint64
 	err = r.db.QueryRow("INSERT INTO person (name) VALUES($1) RETURNING id", dbPerson.Name).Scan(&personId)
-	golog.Debug("personID :", personId)
+	golog.Error("personID :", personId)
 	if err != nil {
 		return err
 	}
