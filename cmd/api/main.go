ec7a36af01b4414d2cb19cf0f1e243f223cd4e8f
Куклин С. И <akiraq@yandex.ru>
2020-03-29 17:56:52 +0300

    fix(app)

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,18 +1,21 @@
 package main
 
 import (
+	"github.com/kataras/golog"
 	"joblessness/haha/server"
 	"joblessness/haha/utils/cors"
 )
 
 func main() {
+	golog.Error("1")
 	corsHandler := cors.NewCorsHandler()
-	
+	golog.Error("2")
 	corsHandler.AddOrigin("https://91.210.170.6:8000")
 	corsHandler.AddOrigin("http://91.210.170.6:8000")
 	corsHandler.AddOrigin("http://localhost:8000")
 	corsHandler.AddOrigin("http://localhost:8080")
 	corsHandler.AddOrigin("https://compassionate-wescoff-a0cb89.netlify.com")
+	golog.Error("3")
 
 	app := server.NewApp(corsHandler)
 	app.StartRouter()
