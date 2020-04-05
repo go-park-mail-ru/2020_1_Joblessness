bf7fe01be0676df10e4c1ebc70fe476e93091470
Куклин С. И <akiraq@yandex.ru>
2020-04-05 03:22:09 +0300

    fix(vacancy): bug in register

--- haha/vacancy/delivery/http/register.go
+++ haha/vacancy/delivery/http/register.go
@@ -14,5 +14,5 @@ func RegisterHTTPEndpoints(router *mux.Router, m *middleware.SessionHandler, uc 
 	router.HandleFunc("/vacancies/{vacancy_id}", h.GetVacancy).Methods("GET")
 	router.HandleFunc("/vacancies/{vacancy_id}", m.OrganizationRequired(h.ChangeVacancy)).Methods("PUT")
 	router.HandleFunc("/vacancies/{vacancy_id}", m.OrganizationRequired(h.DeleteVacancy) ).Methods("DELETE")
-	router.HandleFunc("/organizations/{organization_id}/vacancies", h.GetVacancies ).Methods("GET")
+	router.HandleFunc("/organizations/{organization_id}/vacancies", h.GetOrgVacancies ).Methods("GET")
 }
