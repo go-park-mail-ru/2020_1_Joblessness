d7175b65614a173e7d7af04b65580a4967d16695
Куклин С. И <akiraq@yandex.ru>
2020-04-06 17:35:03 +0300

    bad commit

--- haha/middleware/cors.go
+++ haha/middleware/cors.go
@@ -41,7 +41,8 @@ func (corsList *CorsHandler) PrivateApi (w *http.ResponseWriter, req *http.Reque
 
 	if result {
 		golog.Info("Allowed")
-		(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Origin, Set-Cookie, Access-Control-Allow-Methods, Access-Control-Allow-Credentials")
+		(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Origin, " +
+			"Set-Cookie, Access-Control-Allow-Methods, Access-Control-Allow-Credentials")
 		(*w).Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
 		(*w).Header().Set("Access-Control-Allow-Origin", origin)
 		(*w).Header().Set("Access-Control-Allow-Credentials", "true")
