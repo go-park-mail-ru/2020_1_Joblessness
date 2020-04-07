967884ab087e34259ad91a9e8c5d474c3511a96b
Куклин С. И <akiraq@yandex.ru>
2020-04-07 23:40:19 +0300

    fix: cors

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,11 +8,9 @@ import (
 func main() {
 	corsHandler := middleware.NewCorsHandler()
 
-	corsHandler.AddOrigin("https://5.23.54.85:80")
-	corsHandler.AddOrigin("http://5.23.54.85:80")
+	corsHandler.AddOrigin("http://5.23.54.85")
 	corsHandler.AddOrigin("http://localhost:8080")
 	corsHandler.AddOrigin("http://localhost:80")
-	corsHandler.AddOrigin("https://compassionate-wescoff-a0cb89.netlify.com")
 
 	app := server.NewApp(corsHandler)
 	app.StartRouter()
