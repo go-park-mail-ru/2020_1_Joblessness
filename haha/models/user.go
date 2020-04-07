80d9923d027877b6466e337d9f7eb4353f290f56
maratishimbaev <maratishimbaev8@gmail.com>
2020-04-07 15:08:57 +0500

    Added simple validation

--- haha/models/user.go
+++ haha/models/user.go
@@ -4,26 +4,26 @@ import "time"
 
 type Person struct {
 	ID uint64 `json:"id,omitempty"`
-	Login string `json:"login,omitempty" validate:"required"`
-	Password string `json:"password,omitempty" validate:"required"`
-	Tag string `json:"tag,omitempty"`
-	Email string `json:"email,omitempty"`
-	Phone string `json:"phone,omitempty"`
+	Login string `json:"login,omitempty" validate:"required,max=60"`
+	Password string `json:"password,omitempty" validate:"required,max=60"`
+	Tag string `json:"tag,omitempty" validate:"min=6,max=20"`
+	Email string `json:"email,omitempty" validate:"max=60,email"`
+	Phone string `json:"phone,omitempty" validate:"max=20"`
 	Registered time.Time `json:"registered,omitempty"`
 	Avatar string `json:"avatar,omitempty"`
-	FirstName string `json:"firstName,omitempty"`
-	LastName string `json:"lastName,omitempty"`
-	Gender string `json:"gender,omitempty"`
+	FirstName string `json:"firstName,omitempty" validate:"max=60"`
+	LastName string `json:"lastName,omitempty" validate:"max=60"`
+	Gender string `json:"gender,omitempty" validate:"max=10"`
 	Birthday time.Time `json:"birthday,omitempty"`
 }
 
 type Organization struct {
 	ID uint64 `json:"id,omitempty"`
-	Login string `json:"login,omitempty" validate:"required"`
-	Password string `json:"password,omitempty" validate:"required"`
-	Tag string `json:"tag,omitempty"`
-	Email string `json:"email,omitempty"`
-	Phone string `json:"phone,omitempty"`
+	Login string `json:"login,omitempty" validate:"required,max=60"`
+	Password string `json:"password,omitempty" validate:"required,max=60"`
+	Tag string `json:"tag,omitempty" validate:"min=6,max=20"`
+	Email string `json:"email,omitempty" validate:"max=60"`
+	Phone string `json:"phone,omitempty" validate:"max=20"`
 	Registered time.Time `json:"registered,omitempty"`
 	Avatar string `json:"avatar,omitempty"`
 	Name string `json:"name,omitempty"`
