77d871a0d982bd112cb151192b00d16b83b92f34
Куклин С. И <akiraq@yandex.ru>
2020-04-09 15:19:39 +0300

    feat: subroutes in register

--- haha/summary/delivery/http/register.go
+++ haha/summary/delivery/http/register.go
@@ -8,16 +8,18 @@ import (
 
 func RegisterHTTPEndpoints(router *mux.Router, m *middleware.SessionHandler, uc summaryInterfaces.SummaryUseCase) {
 	h := NewHandler(uc)
+	summariesRouter := router.PathPrefix("/summaries").Subrouter()
+	usersRouter := router.PathPrefix("/users").Subrouter()
 
-	router.HandleFunc("/summaries", m.PersonRequired(h.CreateSummary)).Methods("POST")
-	router.HandleFunc("/summaries", h.GetSummaries).Methods("GET")
-	router.HandleFunc("/summaries/{summary_id:[0-9]+}", h.GetSummary).Methods("GET")
-	router.HandleFunc("/summaries/{summary_id:[0-9]+}", m.PersonRequired(h.ChangeSummary)).Methods("PUT")
-	router.HandleFunc("/summaries/{summary_id:[0-9]+}", m.PersonRequired(h.DeleteSummary)).Methods("DELETE")
-	router.HandleFunc("/summaries/{summary_id:[0-9]+}/print", h.PrintSummary).Methods("GET")
-	router.HandleFunc("/users/{user_id:[0-9]+}/summaries", h.GetUserSummaries).Methods("GET")
+	summariesRouter.HandleFunc("", m.PersonRequired(h.CreateSummary)).Methods("POST")
+	summariesRouter.HandleFunc("", h.GetSummaries).Methods("GET")
+	summariesRouter.HandleFunc("/{summary_id:[0-9]+}", h.GetSummary).Methods("GET")
+	summariesRouter.HandleFunc("/{summary_id:[0-9]+}", m.PersonRequired(h.ChangeSummary)).Methods("PUT")
+	summariesRouter.HandleFunc("/{summary_id:[0-9]+}", m.PersonRequired(h.DeleteSummary)).Methods("DELETE")
+	summariesRouter.HandleFunc("/{summary_id:[0-9]+}/print", h.PrintSummary).Methods("GET")
+	usersRouter.HandleFunc("/{user_id:[0-9]+}/summaries", h.GetUserSummaries).Methods("GET")
 	router.HandleFunc("/vacancies/{vacancy_id:[0-9]+}/response", m.PersonRequired(h.SendSummary)).Methods("POST")
-	router.HandleFunc("/summaries/{summary_id:[0-9]+}/response", m.OrganizationRequired(h.ResponseSummary)).Methods("PUT")
+	summariesRouter.HandleFunc("/{summary_id:[0-9]+}/response", m.OrganizationRequired(h.ResponseSummary)).Methods("PUT")
 	router.HandleFunc("/organizations/{user_id:[0-9]+}/response", h.GetOrgSendSummaries).Methods("GET")
-	router.HandleFunc("/users/{user_id:[0-9]+}/response", h.GetUserSendSummaries).Methods("GET")
+	usersRouter.HandleFunc("/{user_id:[0-9]+}/response", h.GetUserSendSummaries).Methods("GET")
 }
