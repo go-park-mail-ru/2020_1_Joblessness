97608809535a98d42d9f723221b50f4195187e1a
Куклин С. И <akiraq@yandex.ru>
2020-04-23 10:10:32 +0300

    feat: jaeger started

--- cmd/search/main.go
+++ cmd/search/main.go
@@ -2,15 +2,46 @@ package main
 
 import (
 	"github.com/kataras/golog"
+	traceutils "github.com/opentracing-contrib/go-grpc"
+	"github.com/opentracing/opentracing-go"
+	"github.com/uber/jaeger-client-go"
+	jaegercfg "github.com/uber/jaeger-client-go/config"
+	jaegerlog "github.com/uber/jaeger-client-go/log"
+	"github.com/uber/jaeger-lib/metrics"
 	"google.golang.org/grpc"
 	searchPostgres "joblessness/haha/search/repository/postgres"
 	"joblessness/haha/utils/database"
 	searchRpc "joblessness/searchService/rpc"
 	searchServer "joblessness/searchService/server"
+	"log"
 	"net"
 )
 
 func main() {
+	jaegerCfgInstance := jaegercfg.Configuration{
+		ServiceName: "session",
+		Sampler: &jaegercfg.SamplerConfig{
+			Type:  jaeger.SamplerTypeConst,
+			Param: 1,
+		},
+		Reporter: &jaegercfg.ReporterConfig{
+			LogSpans:           true,
+			LocalAgentHostPort: "localhost:6831",
+		},
+	}
+
+	tracer, closer, err := jaegerCfgInstance.NewTracer(
+		jaegercfg.Logger(jaegerlog.StdLogger),
+		jaegercfg.Metrics(metrics.NullFactory),
+	)
+
+	if err != nil {
+		log.Fatal("cannot create tracer", err)
+	}
+
+	opentracing.SetGlobalTracer(tracer)
+	defer closer.Close()
+
 	db, err := database.OpenDatabase()
 	if err != nil {
 		golog.Error(err.Error())
@@ -25,7 +56,7 @@ func main() {
 		return
 	}
 
-	server := grpc.NewServer()
+	server := grpc.NewServer(grpc.UnaryInterceptor(traceutils.OpenTracingServerInterceptor(tracer)))
 	searchRpc.RegisterSearchServer(server, searchServer.NewSearchServer(repo))
 	server.Serve(list)
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.13
 require (
 	github.com/DATA-DOG/go-sqlmock v1.4.1
 	github.com/aws/aws-sdk-go v1.29.24
+	github.com/codahale/hdrhistogram v0.0.0-20161010025455-3a0bb77429bd // indirect
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/golang/mock v1.4.3
 	github.com/golang/protobuf v1.4.0
@@ -18,10 +19,15 @@ require (
 	github.com/lib/pq v1.3.0
 	github.com/microcosm-cc/bluemonday v1.0.2
 	github.com/muesli/regommend v0.0.0-20190807045506-ee89be7688a4
+	github.com/opentracing-contrib/go-grpc v0.0.0-20191001143057-db30781987df
+	github.com/opentracing/opentracing-go v1.1.0
 	github.com/pkg/errors v0.9.1
 	github.com/sendgrid/rest v2.4.1+incompatible // indirect
 	github.com/sendgrid/sendgrid-go v3.5.0+incompatible
 	github.com/stretchr/testify v1.5.1
+	github.com/uber/jaeger-client-go v2.23.0+incompatible
+	github.com/uber/jaeger-lib v2.2.0+incompatible
+	go.uber.org/atomic v1.6.0 // indirect
 	golang.org/x/crypto v0.0.0-20200317142112-1b76d66859c6
 	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e
 	golang.org/x/sys v0.0.0-20200420163511-1957bb5e6d1f // indirect
